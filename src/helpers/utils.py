de39e23d98b2d1f3d09bc52e932ec49a6f22ca24
Ahmed Gad <ahmed.f.gad@gmail.com>
2021-02-04 18:09:25 -0500

    Issue using CPU
    The map_location argument is passed in the torch.load() function. When there is a GPU, then it is assigned to torch.device('cuda'). If no GPU is available, it is assigned to 'cpu'.

--- src/helpers/utils.py
+++ src/helpers/utils.py
@@ -172,7 +172,7 @@ def load_model(save_path, logger, device, model_type=None, model_mode=None, curr
 
     start_time = time.time()
     from src.model import Model
-    checkpoint = torch.load(save_path)
+    checkpoint = torch.load(save_path, map_location=torch.device('cpu'))
     loaded_args_d = checkpoint['args']
 
     args = Struct(**loaded_args_d)
@@ -342,7 +342,7 @@ def log(model, storage, epoch, idx, mean_epoch_loss, current_loss, best_loss, st
         report_f = logger.info   
     else:
         report_f = print
-    
+
     report_f('================>>>')
     report_f(header)
     report_f('================>>>')
